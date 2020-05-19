ba1722c7430deeadb2048e9a27bf8d670ee1782f
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-05-19 10:00:58 -0700

    set up database in cloud

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // set up database 
-mongoose.connect("mongodb://localhost:27017/todoListDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin-kevan:Test123@cluster0-xrmtg.mongodb.net/todoListDB", { useNewUrlParser: true });
 
 // set up schema 
 const itemsSchema = {
@@ -87,10 +87,6 @@ app.post("/", function (req, res) {
       res.redirect("/" + listName)
     })
   }
-
-
-
-
 });
 
 app.post("/delete", (req, res) => {
